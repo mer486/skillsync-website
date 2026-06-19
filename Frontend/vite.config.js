9d95e436704bf4930145525780d7f01722343163
mer486 <merola.g@gmail.com>
2026-06-19 10:01:22 +0300

    railway edits

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,16 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: [
-      'skillsync-website-production.up.railway.app',
-    ],
-  },
-  preview: {
-    host: '0.0.0.0',
-    allowedHosts: [
-      'skillsync-website-production.up.railway.app',
-    ],
-  },
 })
